ec96c20bc3ea18a38e1444573bd338434d576d05
shanmugarajHitHub <shanmugaraj@jovee.in>
2018-01-27 17:06:35 +0530

    Commit

--- Kinship-MobileApp/app/src/main/java/com/example/elcot/kinship2/HomeFragment.kt
+++ Kinship-MobileApp/app/src/main/java/com/example/elcot/kinship2/HomeFragment.kt
@@ -1,23 +1,25 @@
 package com.example.elcot.kinship2
 
+import android.app.FragmentManager
 import android.app.ProgressDialog
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentTransaction
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
 import android.widget.TextView
 import io.reactivex.disposables.Disposable
-import java.util.zip.Inflater
+import kotlinx.android.synthetic.main.fragment_blood_donator_instructions.*
 
 class HomeFragment : Fragment() {
 
-    var mApiInterface : ApiInterface? = null
-    private var mCompositeDisposable : Disposable? = null
-    val progressBar : ProgressBar? = null
+    //var mApiInterface : ApiInterface? = null
+    //private var mCompositeDisposable : Disposable? = null
+   // val progressBar : ProgressBar? = null
     var activity_home_fragment_hospitals_count_TextView : TextView? = null
     var activity_home_fragment_users_count_TextView : TextView? = null
     var activity_home_fragment_donator_count_TextView : TextView? = null
@@ -25,8 +27,16 @@ class HomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         //return super.onCreateView(inflater, container, savedInstanceState)
         var view:View=inflater.inflate(R.layout.activity_home_fragment,container,false)
-        mApiInterface=RetrofitClient.getClient()
-        updatedDetails()
+       // mApiInterface=RetrofitClient.getClient()
+        //updatedDetails()
+        val blooddonatorinstructionsTextView=view.findViewById<TextView>(R.id.activity_home_fragment_bloodDonatorInstructions_textview)
+        blooddonatorinstructionsTextView.setOnClickListener{
+            val fragment = BloodDonatorInstructionsFragment()
+            val fragmentManager = fragmentManager
+            val fragmentTransaction = fragmentManager?.beginTransaction()
+            fragmentTransaction?.replace(R.id.frame_layout,fragment)
+            fragmentTransaction?.commit()
+        }
         return view
     }
 
@@ -35,10 +45,12 @@ class HomeFragment : Fragment() {
 
     }
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_home)
+
+
+
 
     }
 
