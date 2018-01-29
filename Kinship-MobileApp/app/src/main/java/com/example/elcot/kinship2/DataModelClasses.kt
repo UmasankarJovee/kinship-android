416cfd045180401cdbb4f4a738a8a5207f318eb6
shanmugarajHitHub <shanmugaraj@jovee.in>
2018-01-29 16:42:31 +0530

    second Commit

--- Kinship-MobileApp/app/src/main/java/com/example/elcot/kinship2/DataModelClasses.kt
+++
@@ -1,7 +0,0 @@
-package com.example.elcot.kinship2
-
-data class LoginResult(var status_Login : Boolean,var status_value : Int)
-
-data class RegisterResult(var otp : Long,var status : Int,var message : String, var user_id : Int )
-
-data class PasswordResult(var status_password : Boolean, var message: String)
